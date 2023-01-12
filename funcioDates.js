fbab3b6bade52e0316193bdc5e564761308c43d4
P41T0 <120738518+P41T0@users.noreply.github.com>
2023-01-12 18:56:18 +0100

    Add files via upload
    Fitxers modificats

---
+++ funcioDates.js
@@ -0,0 +1,46 @@
+window.onload = function () {
+    let data = new Date();
+    let diaAvui = data.getDate();
+    let elements = document.querySelectorAll(".spanSoftware")
+    for (let i = 0; i < elements.length; i++) {
+        data.setDate((-i + diaAvui - 1));
+        let dia = data.getDate();
+        dia = dia + 1;
+        let mes = data.getMonth();
+        mes = mes + 1;
+        let any = data.getFullYear();
+        if (dia < 10) {
+            dia = '0' + dia;
+        }
+        if (mes < 10) {
+            mes = '0' + mes;
+        }
+        let dataStr = dia + "/" + mes + "/" + any;
+        elements[i].innerText = dataStr;
+        data = new Date();
+
+    }
+
+    data = new Date();
+    mesActual = data.getMonth();
+    diaAvui = data.getDate();
+    let elementsAside = document.querySelectorAll(".spanHardware");
+    for (let j = elementsAside.length - 1; j >= 0; j--) {
+        data.setMonth(-1 + mesActual);
+        data.setDate((-j + diaAvui + 9));
+        let dia = data.getDate();
+        dia++;
+        let mes = data.getMonth();
+        mes++;
+        let any = data.getFullYear();
+        if (dia < 10) {
+            dia = '0' + dia;
+        }
+        if (mes < 10) {
+            mes = '0' + mes;
+        }
+        let dataStrAside = dia + "/" + mes + "/" + any;
+        elementsAside[j].innerText = dataStrAside;
+        data = new Date();
+    }
+}
